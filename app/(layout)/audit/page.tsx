7f9d0f7d6f67bdb6125ac42385cfcae342a1c492
lemurian33 <lemurianagency@gmail.com>
2026-06-19 17:04:46 +0200

    [Fix] clean code

--- app/(layout)/audit/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function page() {
-  return (
-    <div>test page audit</div>
-  );
-}
-
-export default page;
--- app/(layout)/essentiel/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function page() {
-  return (
-    <div>test page Essentiel</div>
-  );
-}
-
-export default page;
