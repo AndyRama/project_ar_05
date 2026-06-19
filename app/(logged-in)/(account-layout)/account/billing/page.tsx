1f693795937620bde444314594bac9dc56b0895f
lemurian33 <lemurianagency@gmail.com>
2026-06-19 17:11:09 +0200

    [Fix] clean code princing-section-v1

--- app/(logged-in)/(account-layout)/account/billing/page.tsx
+++ app/(logged-in)/(account-layout)/account/billing/page.tsx
@@ -4,7 +4,7 @@ import {
   LayoutHeader,
   LayoutTitle,
 } from "@/features/page/layout";
-import { Pricing } from "@/features/plans/pricing-section-V1";
+// import { Pricing } from "@/features/plans/pricing-section-V1";
 import { combineWithParentMetadata } from "@/lib/metadata";
 import { getUserActiveSubscription } from "@/lib/user/get-user-subscription";
 import { UserBilling } from "./user-billing";
@@ -28,7 +28,7 @@ export default async function OrgBillingPage() {
             </LayoutDescription>
           </LayoutHeader>
         </Layout>
-        <Pricing />
+        {/* <Pricing /> */}
       </>
     );
   }
