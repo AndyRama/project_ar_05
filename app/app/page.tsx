5f78989d210957f3e2fdc24ff331eef998a3db86
lemurian33 <lemurianagency@gmail.com>
2026-07-21 10:08:59 +0200

    [Fix] clean code and add uploadthing

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/features/page/layout";
 import { getRequiredUser } from "@/lib/auth/auth-user";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Plus, Salad  } from "lucide-react";
 import { prisma } from "@/lib/prisma";
 import InformationCards from "./_components/information-cards";
 import { WeightProgressChart } from "./_components/weight-progress-chart";
@@ -41,6 +41,14 @@ export default async function RoutePage() {
           <Button className="gap-2 bg-orange-500 hover:bg-orange-400">
             <Plus className="size-4" />
             Nouveau bilan
+          </Button>
+        </Link>
+      </LayoutActions>
+      <LayoutActions>
+        <Link href="/app/plan-alimentaire">
+          <Button className="gap-2 bg-orange-500 hover:bg-orange-400">
+            <Salad  className="size-4" />
+            Plan alimentaire 
           </Button>
         </Link>
       </LayoutActions>
