c049508cf7f297cab352abf142fb6ddcd436bf5c
lemurian33 <lemurianagency@gmail.com>
2026-08-06 10:34:05 +0200

    [Fix] error path link

--- app/app/bilan/complet/page.tsx
+++ app/app/bilan/complet/page.tsx
@@ -10,6 +10,7 @@ import { prisma } from "@/lib/prisma";
 import { getRequiredUser } from "@/lib/auth/auth-user";
 import { MonthlyAuditForm } from "@/features/landing/audit/month/audit-form";
 import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 export default async function NewBilanPage() {
   const user = await getRequiredUser();
